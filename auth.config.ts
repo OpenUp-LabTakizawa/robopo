881fa5bbedfe7ef3fbd476bb4a1060a96ea9450d
kazutan1230 <kazuki.nagai@bnt.benextgroup.jp>
2025-06-18 07:33:49 +0000

    feat: enhance user authentication flow with credential parsing and user fetching -password比較ロジックをAPIに移動。 -認証ロジックを別ファイルに切り出し、auth.config.tsを簡素化。

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,6 @@
-import { BASE_URL } from "@/app/lib/const"
-import bcrypt from "bcryptjs"
+import { fetchUser, parsedCredentials } from "@/app/lib/auth"
 import type { NextAuthConfig, User } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
-import { z } from "zod"
 
 // biome-ignore lint/style/noDefaultExport: Auth.js公式のdocumentに従う。というか、defaultを削除するとコードが通らない。https://authjs.dev/guides/edge-compatibility
 export default {
@@ -17,45 +15,16 @@ export default {
         },
       },
       authorize: async (credentials) => {
-        const parsedCredentials = z
-          .object({
-            username: z.string(),
-            password: z.string(),
-          })
-          .safeParse(credentials)
-        if (parsedCredentials.success) {
-          const { username, password } = parsedCredentials.data
-          // ユーザー名とパスワードを検証
-          const url = `${BASE_URL}/api/user/`
-          try {
-            const response = await fetch(url, {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({ name: username }),
-            })
-            const { user } = await response.json()
-            if (!user) {
-              return null
-            }
-            const passwordMatch = await bcrypt.compare(password, user.password)
-            const authUser: User = {
-              id: user.id.toString(),
-              name: user.name,
-              email: null,
-              image: null,
-            }
-            if (passwordMatch) {
-              return authUser
-            }
-            // 認証に失敗した場合はnullを返す
-            return null
-          } catch (error) {
-            // biome-ignore lint/suspicious/noConsole: errorを何か使わないと怒られるから使っておく。どちらにせよconsoleを使うなと怒られる。
-            console.error("Error during authentication:", error)
+        try {
+          const { username, password } = parsedCredentials(credentials)
+          const user = await fetchUser(username, password)
+          if (!user) {
             return null
           }
+          return user as User
+        } catch {
+          return null
         }
-        return null
       },
     }),
   ],
